c9cc8313c1fcc792b5c47fb0f9a91cc91479c73e
Koushik Ghosh <koushik08oct@gmail.com>
2014-02-24 09:40:22 +0530

    JBPM HelloWorld

---
+++ HelloWorld/src/main/java/com/codestrive/jbpmDemo/DemoRun.java
@@ -0,0 +1,20 @@
+package com.codestrive.jbpmDemo;
+
+import com.sample.ProcessTest;
+
+public class DemoRun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ProcessTest processTest = new ProcessTest();
+		System.out.println("Starting the process ..... ");
+		processTest.testProcess();
+		System.out.println("Prcess End ...");
+
+	}
+
+}
